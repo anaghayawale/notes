c39c1b8f4ce9d59bd4f7dbd69b633410091d12cf
anaghayawale <anaghayawale7@gmail.com>
2023-12-13 18:50:13 +0530

    Updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:notes/providers/user_provider.dart';
+import 'package:notes/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => UserProvider()),
+    ],
+    child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: Constants.yellowColor,
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Constants.yellowColor,
+          selectionColor: Constants.yellowColor.withOpacity(0.5),
+          selectionHandleColor: Constants.yellowColor,
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Constants.yellowColor),
         useMaterial3: true,
       ),
       home: const Scaffold(
