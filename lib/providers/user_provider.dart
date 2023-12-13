f98b1021d8ae8dd790ef52e638c4390b040459c5
anaghayawale <anaghayawale7@gmail.com>
2023-12-13 18:44:07 +0530

    Created user provider

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../models/user.dart';
+
+class UserProvider with ChangeNotifier{
+  User _user = User(
+    id: '', 
+    name: '', 
+    email: '', 
+    password: '', 
+    token: '');
+
+  User get user => _user;
+
+  void setUser(String user){
+    _user = User.fromJson(user);
+    notifyListeners();
+  }
+
+  void setUserFromModel(User user){
+    _user = user;
+    notifyListeners();
+  }
+}
