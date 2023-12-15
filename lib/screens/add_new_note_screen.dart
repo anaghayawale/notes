67ba5a817eab490b68b05732974550b3f3e3efdb
anaghayawale <anaghayawale7@gmail.com>
2023-12-15 16:30:36 +0530

    Added Add New Note Screen

---
+++ lib/screens/add_new_note_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+
+class AddNewNoteScreen extends StatefulWidget {
+  const AddNewNoteScreen({super.key});
+
+  @override
+  State<AddNewNoteScreen> createState() => _AddNewNoteScreenState();
+}
+
+class _AddNewNoteScreenState extends State<AddNewNoteScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _noteController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Constants.yellowColor,
+        title: Text("Add Note", style: TextStyle(
+            fontWeight: FontWeight.w400,
+            color: Constants.yellowColor,
+          ),
+        ),
+        
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                autofocus: true,
+                onSubmitted: (value) {
+                },
+                style: const TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                cursorColor: Constants.yellowColor,
+                decoration: const InputDecoration(
+                  hintText: "Title",
+                  border: InputBorder.none,
+                ),
+              ),
+              Divider(
+                thickness: 1.0,
+                color: Constants.yellowColor,
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _noteController,
+                  maxLines: null,
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                  ),
+                  cursorColor: Constants.yellowColor,
+                  decoration: const InputDecoration(
+                    hintText: "Start typing...",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes/screens/add_new_note_screen.dart';
 import 'package:notes/utils/constants.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,7 +13,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    UserProvider userProvider = Provider.of<UserProvider>(context);
     return Scaffold(
       backgroundColor: Constants.appBackgroundColor,
       appBar: AppBar(
@@ -27,11 +25,38 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Text('Welcome to ${userProvider.user.name}\'s notes'),
-        ],
-      ),
+      body: SafeArea(
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2), 
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 220.0,
+              width: 200.0,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+              padding: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 12.0),
+              decoration: BoxDecoration(
+                color: Constants.whiteColor,
+                borderRadius:BorderRadius.circular(20.0),
+            ),
+            );
+          },),),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Constants.yellowColor,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+        onPressed: () {
+          Navigator.push(context, 
+          CupertinoPageRoute(
+            fullscreenDialog: true,
+            builder: (context) => const AddNewNoteScreen(),),);
+        },
+        child: Icon(Icons.add, color: Constants.whiteColor),
+
+      )
     );
   }
 }
